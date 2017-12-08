177d267c3f0231aa9279b704cdeec46532b70c63
Anish Chakraborty <anish.chakraborty@philips.com>
2017-12-08 21:13:43 +0530

    tested on aws ec2

--- src/main/resources/local/15_IngestData.sh
+++ src/main/resources/local/15_IngestData.sh
@@ -2,7 +2,7 @@
 
 [ $# -ne 2 ] && { echo "Usage: $0 local_omniture_source_dir local_sales_source_dir" ; exit 1; }
 
-hadoop_success_file=/data/raw/_SUCCESS
+hadoop_success_file=/user/ec2-user/data/raw/_SUCCESS
 
 # Check if a success file is present, which means the previously loaded data has not been ingested yet.
 if $(hdfs dfs -test -f $hadoop_success_file) ; then { echo "Earlier data not ingested yet"; exit 1; }; fi
@@ -13,7 +13,7 @@ ts=$(date "+%Y%m%d_%H") # Precise to hour. Add %M%S to make it more precise
 echo "Time Stamp used is $ts"
 
 local_omniture_source_dir=$1
-hadoop_omniture_data_dir=/data/raw/omniture/$ts
+hadoop_omniture_data_dir=/user/ec2-user/data/raw/omniture/$ts
 
 hdfs dfs -mkdir -p $hadoop_omniture_data_dir && \
 hdfs dfs -put $local_omniture_source_dir $hadoop_omniture_data_dir && \
@@ -22,7 +22,7 @@ echo "Successfully pushed Omniture data from $local_omniture_source_dir to $hado
 omnitureLoad=$?
 
 local_sales_source_dir=$2
-hadoop_sales_data_dir=/data/raw/sales/$ts
+hadoop_sales_data_dir=/user/ec2-user/data/raw/sales/$ts
 
 hdfs dfs -mkdir -p $hadoop_sales_data_dir && \
 hdfs dfs -put $local_sales_source_dir $hadoop_sales_data_dir && \
--- src/main/resources/setupHdfs.sh
+++ src/main/resources/setupHdfs.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 
-hdfsCodeDir=/code
+hdfsCodeDir=/user/ec2-user/code
 
 hdfs dfs -rm -r $hdfsCodeDir
 hdfs dfs -mkdir -p $hdfsCodeDir && \
 hdfs dfs -put `dirname $0`/hdfs/* $hdfsCodeDir && \
 echo "Deployed codes to $hdfsCodeDir"
 
-sudo ln -fs /usr/lib/hive/conf/hive-site.xml /usr/lib/spark/conf/hive-site.xml && \
-echo "Linked hive-site.xml to spark conf"
+#sudo ln -fs /usr/lib/hive/conf/hive-site.xml /usr/lib/spark/conf/hive-site.xml && \
+#echo "Linked hive-site.xml to spark conf"
 
-hdfs dfs -put /usr/lib/hive/conf/hive-site.xml /code/hive/hive-site.xml && \
+#hdfs dfs -put /usr/lib/hive/conf/hive-site.xml /code/hive/hive-site.xml && \
+hdfs dfs -put /etc/hive/conf/hive-site.xml /user/ec2-user/code/hive/ && \
 echo "Copied hive-site.xml to HDFS for Oozie"
 
